a1b1536c9383212ee68639275b4262539e75e377
skouch <skouchy24@gmail.com>
2023-06-26 00:43:24 -0400

    fest(class): classes referencing successfully, validations working, ALL GOOD

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { error } = require('console');
+const { Circle, Square, Triangle } = require('./lib/shapes');
+const validateColor = require('validate-color').default;
 
-// class LogoContent {
-//     constructor(shape, shapeColor, logoTxt, colorTxt) {
-//         this.shape = shape;
-//         this.shapeColor = shapeColor;
-//         this.logoTxt = logoTxt;
-//         this.colorTxt = colorTxt;
-//     }
-// }
 
+// Create and SVG string based on user input
 const generateSVG = userInput => {
+    const size = 80;
+    let shape;
+
+    switch (userInput.shape) {
+        case 'circle':
+            shape = new Circle(userInput.color, size).draw(size / 2 + 10, size / 2 + 10);
+            break;
+        case 'triangle':
+            shape = new Triangle(userInput.color, size).draw(10, 10);
+            break;
+        case 'square':
+        default:
+            shape = new Square(userInput.color, size).draw(10, 10);
+            break;
+    }
+
+    const logoText = `
+      <text x="${size / 2 + 10}" y="${size / 1.5}" text-anchor="middle" fill="${userInput.textColor}" font-size="60">${userInput.logo_text}</text>
+    `;
+
     return `
-    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <rect x="10" y="10" width="80" height="80" fill="${userInput.color}" />
-    <text x="50" y="55" text-anchor="middle" fill="white" font-size="60">${userInput.logo_text}</text>
-  </svg>`;
+      <svg viewBox="0 0 ${size + 20} ${size + 20}" xmlns="http://www.w3.org/2000/svg">
+        ${shape}
+        ${logoText}
+      </svg>
+    `;
 };
+
 // user prompts for custom logo details & generates SVG file
 const generateLogo = () => {
     inquirer.prompt([
@@ -25,35 +42,34 @@ const generateLogo = () => {
             type: 'input',
             name: 'logo_text',
             message: 'Choose up to 3 characters that your logo will contain (Required)',
-            validate: logoInput => {
-                // * validate that there are not more than 3 chars input by the user * //
-                if (logoInput.length > 3) {
-                    console.log('Logo text can only be three characters!');
-                    return false;
-                } else {
+            validate: textInput => {
+                if (textInput.length <= 3) {
+                    console.log(textInput.length);
                     return true;
+                } else {
+                    console.log('Please limit Text to 3 characters Max');
+                return false;
                 }
             }
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'What color would you like the TEXT to be? (Required)',
+            validate: validateColor
         },
         {
             // * use list type so only one option can be selected * //
             type: 'list',
             name: 'shape',
-            message: 'Select a shape doooood:',
+            message: 'Select a shape for your sweet logo doooood:',
             choices: ['circle', 'triangle', 'square']
         },
         {
             type: 'input',
             name: 'color',
-            message: 'Please enter a color keyword (OR a hexadecimal number)! (Required)',
-            validate: colorInput => {
-                if (colorInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a color keyword (OR a hexadecimal number)... bitch');
-                    return false;
-                }
-            }
+            message: 'What color would you like the SHAPE to be? (Required)',
+            validate: validateColor
         },
     ]).then(userInput => {
         console.log(userInput);
